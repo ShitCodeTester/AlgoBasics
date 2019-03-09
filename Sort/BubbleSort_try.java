e26c4744dee5eb95bc177094c20da911aee39c4d
ShitCodeTester <48391124+ShitCodeTester@users.noreply.github.com>
2019-03-09 21:10:14 +0200

    BubbleSort_try.java added - initial implementation
    Initial implementation by Trainee_0

---
+++ Sort/BubbleSort_try.java
@@ -0,0 +1,35 @@
+package Algos;
+
+public class BubbleSort_try {
+
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random()*50);
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int g = 0; g < i; g++) {
+                if (arr[g] > arr[g + 1]) {
+                    int min = arr[g + 1];
+                    arr[g + 1] = arr[g];
+                    int min_i = g;
+                    arr[g] = min;
+                }
+            }
+        }
+        
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+    }
+    
+}
